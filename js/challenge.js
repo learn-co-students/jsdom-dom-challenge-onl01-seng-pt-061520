451cdfe4b0143fefa562b8fe85c845a619028a9b
Jessie <jssdnvn.com>
2020-12-25 21:08:14 -0600

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,13 +1,25 @@
 // As a user, I should see the timer increment every second once the page has loaded.
 
 // As a user, I can manually increment and decrement the counter using the plus and minus buttons.
-const plus = document.getElementById('plus')
+const counter = document.getElementById('counter');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const heart = document.getElementById('heart');
+const pause = document.getElementById('pause');
 
-plus.addEventListener("click", function(event) {
+let count = 0
 
-}
+plus.addEventListener("click", function() {
+ counter.innerText = `${count ++}`;
+});
+
+minus.addEventListener("click", function() {
+ counter.innerText = `${count --}`;
+});
 
 // As a user, I can 'like' an individual number of the counter. I should see count of the number of 'likes' associated with that number.
-
+heart.addEventListener("click", function() {
+ counter.appendChild() = "liked";
+});
 // As a user, I can pause the counter, which should
 
