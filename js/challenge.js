4151359ea8c445f9516d4ea2a10614d2d4f3765b
Jessie <jssdnvn.com>
2020-12-25 20:57:16 -0600

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,13 @@
+// As a user, I should see the timer increment every second once the page has loaded.
+
+// As a user, I can manually increment and decrement the counter using the plus and minus buttons.
+const plus = document.getElementById('plus')
+
+plus.addEventListener("click", function(event) {
+
+}
+
+// As a user, I can 'like' an individual number of the counter. I should see count of the number of 'likes' associated with that number.
+
+// As a user, I can pause the counter, which should
+
