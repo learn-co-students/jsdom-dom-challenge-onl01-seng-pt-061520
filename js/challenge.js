95bbf7c7e49a7c43de399bdcc42d340e43b02b99
Bryce B <bryce.boddy@gmail.com>
2021-01-07 09:53:53 +0000

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,11 +1,20 @@
-var counter = document.getElementById('counter')
+let counter = document.getElementById('counter');
 let number = 0;
-var paused = false;
+let paused = false;
 
-var minus = document.getElementById('minus');
-var plus = document.getElementById('plus');
-var heart = document.getElementById('heart');
-var pause = document.getElementById('pause');
+let minus = document.getElementById('minus');
+let plus = document.getElementById('plus');
+let heart = document.getElementById('heart');
+let pause = document.getElementById('pause');
+
+document.getElementById("submit").addEventListener("click", function(event){
+  event.preventDefault();
+  comment = document.getElementById('comment-input').value;
+  var ul = document.getElementById('allComments');
+  var h4 = document.createElement('h4');
+  h4.appendChild(document.createTextNode(comment));
+  ul.appendChild(h4);
+});
 
 function subtractTimer(){
   number--;
@@ -19,16 +28,13 @@ function addTimer() {
 
 function stopTimer(){
   paused = !paused;
-  alert(`Stopped Timer (${paused})`);
 }
 
 function likeNumber(){
-  let ul = document.getElementById('likes')
-  let li = document.createElement('li');
+  var ul = document.getElementById('likes');
+  var li = document.createElement('li');
   li.appendChild(document.createTextNode(number));
   ul.appendChild(li);
-  li.innerHTML = number;
-  console.log("poop")
 }
 
 function startGame(){
