033e9ab463daa20c78ac1b49e2212162246d440b
Bryce B <bryce.boddy@gmail.com>
2021-01-07 05:07:20 +0000

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -7,9 +7,28 @@ var plus = document.getElementById('plus');
 var heart = document.getElementById('heart');
 var pause = document.getElementById('pause');
 
+function subtractTimer(){
+  number--;
+  counter.innerHTML = number;
+}
+
+function addTimer() {
+  number ++;
+  counter.innerHTML = number;
+}
+
 function stopTimer(){
   paused = !paused;
   alert(`Stopped Timer (${paused})`);
+}
+
+function likeNumber(){
+  let ul = document.getElementById('likes')
+  let li = document.createElement('li');
+  li.appendChild(document.createTextNode(number));
+  ul.appendChild(li);
+  li.innerHTML = number;
+  console.log("poop")
 }
 
 function startGame(){
